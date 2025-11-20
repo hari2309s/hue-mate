d520d8752cd778e5234805656d12e2bba9696439
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-11-20 23:14:58 +0530

    [packages] -- feat(ui): add button and card components with styling utilities
    - Add Button component with variants and sizes - Add Card component with
    subcomponents (Header, Title, etc.) - Add cn utility for class name
    merging - Update tsconfig for react-jsx support

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,12 @@
 export { ColorSwatch } from "./components/color-swatch";
+export { Button, type ButtonProps } from "./components/button";
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  type CardProps,
+} from "./components/card";
+export { cn } from "./lib/utils";
